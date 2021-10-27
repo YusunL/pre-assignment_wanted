18fc2f2b640ebed3cf6755d5d6727b445136f8a3
YusunL <47683668+YusunL@users.noreply.github.com>
2021-10-27 13:10:38 +0900

    writing views(not_finished)

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import (
+    ListView,
+    FormView,
+)
+from django.shortcuts import render, redirect, reverse
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from users import mixins as user_mixins
+from . import models, forms
 
-# Create your views here.
+
+class HomeView(ListView):
+
+    """HomeView Definition"""
+
+    model = models.Post
+    paginate_by = 12
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "posts"
+
+
+@login_required
+class WritePostView(user_mixins.LoggedInOnlyView, FormView):
+
+    form_class = forms.WritePostForm
+    template_name = "posts/post_write.html"
+
+    def form_valid(self, form):
+        post = form.save()
+        post.writer = self.request.user
+        post.save()
+        form.save_m2m()
+        messages.success(self.request, "Post Uploaded")
+        return redirect(reverse("core:home"))
